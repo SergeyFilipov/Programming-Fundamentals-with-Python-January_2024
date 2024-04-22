0b8459eff326bfbd5062b248a92384e23066a253
SergeyFilipov <156571377+SergeyFilipov@users.noreply.github.com>
2024-04-22 09:55:35 +0300

    Add files via upload

---
+++ 14.2_final_exam_preparation-2_22_03_2024/02_ad_astra.py
@@ -0,0 +1,16 @@
+import re
+
+food_information = input()
+pattern = r"([#|])([a-zA-Z\s]+)\1(\d{2}\/\d{2}\/\d{2})\1(\d+)\1"
+matches = re.findall(pattern, food_information)
+
+total_calories = sum([int(match[3]) for match in matches])
+
+days = total_calories // 2000
+print(f"You have food to last you for: {days} days!")
+
+for match in matches:
+    item_name = match[1]
+    expiration_date = match[2]
+    calories = match[3]
+    print(f"Item: {item_name}, Best before: {expiration_date}, Nutrition: {calories}")
